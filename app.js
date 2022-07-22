179596cc1b460135bb30b04266d3d73d270e7849
Laith alnatour <laithalnatour92@gmail.com>
2022-07-22 16:12:19 +0300

    solve task arrow functions

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const theOldWay = function(course) {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('The old way:', theOldWay('Code 301'));
+//console.log('The old way:', theOldWay('Code 301'));
 
 
 // STEP 2
@@ -107,43 +107,62 @@ const newObject = array => ({
 // Write your solutions on a single line wherever possible.
 
 
-
+/*
 let sum = function(a, b, c, d) {
   return a + b + c + d;
-};
+}; */
+
+let sum = (a, b, c, d) =>  a + b + c + d;
+
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // console.log(sum(1, 2, 3, 4));
 
-
+/*
 let objectLit = function() {
   return {
     key1: 'value1',
     key2: 'value2',
     key3: 'value3',
   };
-};
+};*/
+
+let objectLit = () => ({
+
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3',
+ 
+});
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(objectLit());
+//console.log(objectLit());
 
-
+/*
 let sumAndProduct = function(a, b) {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
-};
+};*/
+
+let sumAndProduct = (a, b) => {
+  let sum = a + b;
+  let product = a * b;
+  return [sum, product];}
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // console.log(sumAndProduct(3, 9));
 
-
+/*
 let message = function(name) {
   return `Hello, ${name}!`;
-};
+};*/
+
+let message = (name) =>  `Hello, ${name}!`
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+ //console.log(message('Allie'));
 
 
 let Student = function(name, age, hometown) {
@@ -153,6 +172,14 @@ let Student = function(name, age, hometown) {
 };
 
 let joe = new Student('Joe Schmoe', 100, 'Anytown, USA');
+/*
+let Student = (name, age, hometown) => {
+  this.name = name;
+  this.age = age;
+  this.hometown = hometown;
+};
+
+let joe = new Student('Joe Schmoe', 100, 'Anytown, USA');*/
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
@@ -168,18 +195,20 @@ Student.prototype.greeting = function() {
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+ //console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
 
-
+/*
 Student.courseName = function() {
   return 'This student is enrolled in Code 301.';
-};
+};*/
+
+Student.courseName = () => 'This student is enrolled in Code 301.';
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+ //console.log(Student.courseName());
 
 
 
@@ -190,17 +219,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+//joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+ //joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+//it invoked objects elements (the object we are built it)
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+//it invoked the global window object
 // 3. Explain why "this" is different when an arrow function is used.
-//
+//arrow functions retain the this value of the enclosing functional scope ,so best to avoid using an arrow function in a constructor
---
+++ test.js
@@ -0,0 +1,5 @@
+/*const theOldWay = function(course) {
+    return `I am currently enrolled in ${course}`;
+  };
+*/
+  const theOldWay = course =>  `I am currently enrolled in ${course}`;
